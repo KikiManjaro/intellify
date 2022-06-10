6bcf21a71e90a5e64b46c256c1cb7a30df0a1f80
GitHub Action <action@github.com>
2022-06-10 20:44:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/kikimanjaro/intellijspotify/MyBundle.kt
+++ src/main/kotlin/com/github/kikimanjaro/intellijspotify/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kikimanjaro.intellijspotify
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/kikimanjaro/intellijspotify/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kikimanjaro.intellijspotify.services
+
+import com.github.kikimanjaro.intellijspotify.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
